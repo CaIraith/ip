c994a3febbf12ac21e631a6c39b3db49aa1a4c8a
CaLraith <hm.leewenxin@gmail.com>
2026-01-25 19:36:05 +0800

    Implement interactive Duke that echoes commands and exits on 'bye'

--- src/main/java/Sol.java
+++ src/main/java/Sol.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class Sol {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         String logo =
                   " / ___|  ___ | |\n"
                 + " \\___ \\ / _ \\| |\n"
@@ -8,6 +11,16 @@ public class Sol {
 
         System.out.println("Hello. I am\n" + logo);
         System.out.println("What would you like to do?");
-        System.out.println("Goodbye.");
+        String input = "";
+        while (true) {
+            input = scanner.nextLine();
+            if (input.equals("bye")) {
+                break;
+            } else {
+                System.out.println("You said: " + input);
+            }
+        }
+        System.out.println("Goodbye. See you next time.");
+        scanner.close();
     }
 }
