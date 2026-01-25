ba34cf80893b892d4ed4b95b10c3e803628abf5c
CaLraith <hm.leewenxin@gmail.com>
2026-01-25 21:20:43 +0800

    Add support for ToDo, Deadline, and Event task types

--- src/main/java/Sol.java
+++ src/main/java/Sol.java
@@ -35,10 +35,31 @@ public class Sol {
                 tasks[idx].markUndone();
                 System.out.println("Marked the below task as incomplete.");
                 System.out.println("   " + tasks[idx].toString());
-            }
+            } 
             else {
-                System.out.println("Added: " + input);
-                tasks[index] = new Task(input);
+                String[] parts = input.split(" ", 2);
+                String type = parts[0];
+                String description = parts.length > 1 ? parts[1] : "";
+                Task task;
+
+                if (type.equals("todo")) {
+                    task = new ToDo(description);
+                } else if (type.equals("deadline")) {
+                    String[] deadlineParts = description.split(" /by ", 2);
+                    task = new Deadline(deadlineParts[0], deadlineParts[1]);
+                } else if (type.equals("event")) {
+                    String[] eventParts1 = description.split(" /from ", 2);
+                    String desc = eventParts1[0];
+                    String[] eventParts2 = eventParts1[1].split(" /to ", 2);
+                    String start = eventParts2[0];
+                    String end = eventParts2[1];
+                    task = new Event(desc, start, end);
+                } else {
+                    task = new Task(input);
+                }
+                tasks[index] = task;
+                System.out.println("Added: " + tasks[index].toString());
+                System.out.println("You have " + index + " tasks in this list.");
                 index++;
             }
         }
