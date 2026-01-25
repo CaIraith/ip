48b62245140e25fec046bb9f76a4b75f8622a8b8
CaLraith <hm.leewenxin@gmail.com>
2026-01-25 19:13:15 +0800

    Rename chatbot and add initial skeleton

---
+++ src/main/java/Sol.java
@@ -0,0 +1,13 @@
+public class Sol {
+    public static void main(String[] args) {
+        String logo =
+                  " / ___|  ___ | |\n"
+                + " \\___ \\ / _ \\| |\n"
+                + "  ___) | (_) | |\n"
+                + " |____/ \\___/|_|\n";
+
+        System.out.println("Hello. I am\n" + logo);
+        System.out.println("What would you like to do?");
+        System.out.println("Goodbye.");
+    }
+}
